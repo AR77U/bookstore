0b8b5877fa27a739c192ac66304a3cceb27b1fc5
AR77U <bhz983@myy.haaga-helia.fi>
2026-09-03 14:18:31 +0300

    projekti gittiin ja repo luotu

---
+++ src/main/java/book/store/bookstore/domain/BookController.java
@@ -0,0 +1,16 @@
+package book.store.bookstore.domain;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class BookController {
+    @GetMapping("/index")
+    public String getBookName(@RequestParam String param) {
+        return "Kirja";
+    }
+    
+
+}

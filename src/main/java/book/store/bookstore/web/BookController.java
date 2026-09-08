b2a6bf2248dce273f93a158e2098c55f8d88af46
AR77U <bhz983@myy.haaga-helia.fi>
2026-09-08 18:22:13 +0300

    chapter 3, tehtävä 3 muutokset

--- src/main/java/book/store/bookstore/web/BookController.java
+++ src/main/java/book/store/bookstore/web/BookController.java
@@ -1,10 +1,15 @@
 package book.store.bookstore.web;
 
+import book.store.bookstore.domain.Book;
 import book.store.bookstore.domain.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -22,6 +27,26 @@ public class BookController {
         model.addAttribute("booklist" , bookRepository.findAll());
         return "booklist";
     }
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable ("id")Long bookId, Model model) {
+        bookRepository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+    @PostMapping("/save")
+    public String savebook(Book book) {
+        bookRepository.save(book);
+        //TODO: process POST request
+        
+        return "redirect:/booklist";
+    }
+    
+    
+    
     
     
 

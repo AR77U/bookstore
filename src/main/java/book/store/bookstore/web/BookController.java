4cc6f43e06180931ec4c7f416a171af11ef3505c
AR77U <bhz983@myy.haaga-helia.fi>
2026-09-10 13:00:57 +0300

    chapter 3, osa 4

--- src/main/java/book/store/bookstore/web/BookController.java
+++ src/main/java/book/store/bookstore/web/BookController.java
@@ -5,6 +5,7 @@ import book.store.bookstore.domain.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,20 @@ public class BookController {
     }
     @PostMapping("/save")
     public String savebook(Book book) {
+        bookRepository.save(book);
+        //TODO: process POST request
+        
+        return "redirect:/booklist";
+    }
+    
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable ("id")Long bookId, Model model) {
+        Book book = bookRepository.findById(bookId).orElseThrow();
+        model.addAttribute("book", book);
+        return "editbook";
+    }
+    @PostMapping("/edit/save")
+    public String saveBookEdit(@ModelAttribute Book book) {
         bookRepository.save(book);
         //TODO: process POST request
         
